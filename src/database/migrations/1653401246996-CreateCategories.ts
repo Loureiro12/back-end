bf0697019db883ec7a8d57dfaeb7d48f17dd91f0
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-05-24 11:14:41 -0300

    correcao

--- src/database/migrations/1653401246996-CreateCategories.ts
+++ src/database/migrations/1653401246996-CreateCategories.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateCategories1653229622377 implements MigrationInterface {
+export class CreateCategories1653401246996 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
