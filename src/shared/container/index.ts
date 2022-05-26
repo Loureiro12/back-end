2ef1860b74195439752cb3c3abd781f19303665b
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-05-25 21:21:04 -0300

    Refatorando as especificações

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/IcategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificatiosRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationRepository",
+  SpecificationRepository
 );
